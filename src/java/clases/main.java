1bd26c2b4a4cc6d3e22f8a79bdc1406308b0b465
nicoinde <nicoinde@gmail.com>
2018-05-14 21:29:35 -0300

    porque mierda cuesta tanto comitear en esta cagada

---
+++ src/java/clases/main.java
@@ -0,0 +1,59 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author dlcusr
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        /*
+        ArrayList al = new ArrayList();
+        
+        Termino t1 = new Termino();
+        Termino t2 = new Termino();
+        Termino t3 = null;
+        
+        t1.setPalabra("Hola");
+        t2.setPalabra("Puta");
+        
+        al.add(t1);
+        al.add(t2);
+        
+        System.out.println(al.toString());
+        
+        t3 = (Termino)al.get(0);
+        
+        t3.setPalabra("Chau");
+        System.out.println(al.toString());
+        */
+//        Indexacion v = new Indexacion();
+//        //v.armar_vocabulario();
+//        System.out.println("Armando vocabulario...");
+//        v.armar_vocabulario();
+//        System.out.println("Armando posteo...");
+//        try
+//        {
+//            v.armar_posteo();
+//        }
+//        catch(Exception e)
+//        {
+//            System.out.println(e.getMessage());
+//        }
+    }
+}
+
